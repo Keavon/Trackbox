9bbf0693c43c317d9652e99f93b6ce920c56b2ab
Keavon Chambers <keavon@keavon.com>
2014-07-23 20:21:40 -0700

    Added Songs, Artists, and Tags pages.

--- themes/pages/headers/songs.js
+++
@@ -1,7 +0,0 @@
-var list;
-$.getScript("themes/trackbox/frames/data.js", function (data) {
-	list = listings.songs;
-	$("#list-frame").html(function () {
-		return new EJS({ url: "themes/trackbox/frames/song/song.ejs" }).render();
-	});
-});
