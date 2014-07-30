4fdf7c7f080d98d140fdd8951603a0ca46980d79
Keavon Chambers <keavon@keavon.com>
2014-07-30 15:13:18 -0700

    Fixed last commit.

--- packages/songs/songs.js
+++ packages/songs/songs.js
@@ -3,17 +3,23 @@
 library.sort(tb.sortByPriority("+album", "+disk", "+track", "+title"));
 
 tb.getFileContents("packages/songs/templates/list-row.html", function (data) {
-	for (var songs in library) {
-		library[songs].title = library[songs].title || "";
-		library[songs].artists[0] = library[songs].artists[0] || "";
-		library[songs].album = library[songs].album || "";
-		library[songs].year = library[songs].year || "";
-		library[songs].track = library[songs].track || "";
-		library[songs].disk = library[songs].disk || "";
-		library[songs].time = library[songs].time || "";
+	for (var song in library) {
+		library[song].title = library[song].title || "";
+		library[song].artists[0] = library[song].artists[0] || "";
+		library[song].album = library[song].album || "";
+		library[song].year = library[song].year || "";
+		library[song].track = library[song].track || "";
+		library[song].disk = library[song].disk || "";
+		library[song].time = library[song].time || "";
 
-		tb.renderTextTemplate(data, { "TITLE": library[songs].title, "NUMBER": library[songs].track, "ALBUM": library[songs].album, "ARTIST": library[songs].artists[0], "TIME": tb.formatTime(library[songs].time) }, function (template) {
+		tb.renderTextTemplate(data, { "ID" : library[song].id, "TITLE": library[song].title, "NUMBER": library[song].track, "ALBUM": library[song].album, "ARTIST": library[song].artists[0], "TIME": tb.formatTime(library[song].time) }, function (template) {
 			$("#list-frame").append(template);
 		});
 	}
+});
+
+
+$("#list-frame").dblclick(function(event){
+	event.preventDefault();
+	alert(event.target.id);
 });
