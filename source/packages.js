d2d83470c5b1f5107b255fd7e48e2d4df2e61824
joshua-anderson <j@zatigo.com>
2014-08-22 18:03:27 -0700

    Added tb.isPackageManifestValid(manifest)

--- source/packages.js
+++ source/packages.js
@@ -15,3 +15,43 @@ tb.packageStartup = function () {
 		});
 	}
 };
+
+// Check a package manifest to make sure it is valid
+tb.isPackageManifestValid = function (manifest) {
+	if (typeof manifest !== 'object') {
+		console.error("Manifest must be a valid JSON object.");
+		return false;
+	}
+
+	if (!('name' in manifest)) {
+		console.error("'name' key required.");
+		return false;
+	}
+
+	if (manifest.type === "page") {
+		if (!('pageName' in manifest)) {
+			console.error("'pageName' key required.");
+			return false;
+		}
+
+		if (!('page' in manifest)) {
+			console.error("'page' key required.");
+			return false;
+		}
+
+		if (!('url' in manifest)) {
+			console.error("'url' key required.");
+			return false;
+		}		
+	} else if (manifest.type === "shell") {
+		if (!('shell' in manifest)) {
+			console.error("'shell' key required.");
+			return false;
+		}
+	} else {
+		console.error("You must set a valid package type (page, shell) in the manifest file.");
+		return false;
+	}
+
+	return true;
+};
