e420e5f3db74960e3374a21c83eeae9320b48ecb
joshua-anderson <j@zatigo.com>
2014-08-25 20:25:08 -0700

    Begin adding tb.loadPackages

--- source/files.js
+++ source/files.js
@@ -1,7 +1,20 @@
-﻿tb.getFileContents = function (path, callback) {
+﻿// Loads a file via AJAX
+tb.getFileContents = function (path, callback) {
 	$.ajax({
 		"url": path,
 		"cache": false,
 		"dataType": "text"
 	}).done(callback);
+};
+
+// Loads a json file via AJAX
+tb.getJSONFileContents = function (path, callback) {
+	$.ajax({
+		"url": path,
+		"cache": false,
+		"dataType": "json"
+	}).done(callback)
+	.fail(function () {
+		console.error("Error parsing JSON file at \'" + path + "\'.");
+	});
 };
