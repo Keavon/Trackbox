2fb63d6a689e653928e9997a891624e7a69e6d68
joshua-anderson <j@zatigo.com>
2014-09-28 18:34:46 -0700

    Add onPreferencesLoaded event

--- source/preferences.js
+++ source/preferences.js
@@ -3,10 +3,21 @@ tb.loadPreferences = function(forceReload) {
 if(!tb.private.preferences || forceReload) {
 		tb.getJSONFileContents("users/default/preferences.json", function(data) {
 			tb.private.preferences = data;
+			tb.triggerOnPreferencesLoaded();
 		});
 	}
 };
 
 tb.preferences = function(){
 	return tb.copyJSON(tb.private.preferences);
+};
+
+tb.triggerOnPreferencesLoaded = function() {
+	$.event.trigger({"type": "onPreferencesLoaded"});
+};
+
+tb.onPreferencesLoaded = function(callback) {
+	$(window).on("onPreferencesLoaded", function() {
+		callback();
+	});
 };
