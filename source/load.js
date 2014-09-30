268ae3260b4807a4dd74c49f0746580ca26c967e
joshua-anderson <j@zatigo.com>
2014-09-29 19:44:16 -0700

    Added package loading to startup. Bug: Trackbox package is loaded at the same time so it appears twice

--- source/load.js
+++ source/load.js
@@ -1,3 +1,10 @@
 // Bind hash change and start application
 window.onhashchange = tb.router;
 tb.loadShell("Trackbox");
+
+tb.loadPreferences();
+tb.loadPackages();
+
+tb.onPreferencesLoaded(function() {
+	tb.loadShellPackage();
+});
